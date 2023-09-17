df8113ee0fe0dd2a254a24d54f8abe135d0f3df2
Nico <nico.hex6@gmail.com>
2023-09-17 22:46:02 +0200

    Fixed shift-click problem

--- src/main/java/io/github/hexagonnico/elytras/items/ModElytraItem.java
+++ src/main/java/io/github/hexagonnico/elytras/items/ModElytraItem.java
@@ -2,15 +2,9 @@ package io.github.hexagonnico.elytras.items;
 
 import io.github.hexagonnico.elytras.ElytraSkinsMod;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ModElytraItem extends ElytraItem {
 
@@ -26,24 +20,7 @@ public class ModElytraItem extends ElytraItem {
     }
 
     @Override
-    public boolean canEquip(ItemStack stack, EquipmentSlot armorType, Entity entity) {
-        return armorType == EquipmentSlot.CHEST;
-    }
-
-    // TODO: Item can't be shift-clicked into chestplate slot
-
-    @Override
-    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level world, Player player, @NotNull InteractionHand hand) {
-        ItemStack itemstack = player.getItemInHand(hand);
-        if(player.getItemBySlot(EquipmentSlot.CHEST).isEmpty()) {
-            player.setItemSlot(EquipmentSlot.CHEST, itemstack.copy());
-            if(!world.isClientSide()) {
-                player.awardStat(Stats.ITEM_USED.get(this));
-            }
-            itemstack.setCount(0);
-            return InteractionResultHolder.sidedSuccess(itemstack, world.isClientSide());
-        } else {
-            return InteractionResultHolder.fail(itemstack);
-        }
+    public @Nullable EquipmentSlot getEquipmentSlot(ItemStack stack) {
+        return EquipmentSlot.CHEST;
     }
 }
